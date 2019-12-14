5ccdc298f33ddf187bf08421b1dc09e65a157517
emirnur <nur.emirlan@gmail.com>
2019-12-14 13:02:04 +0600

    add models Photo, Comment

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+COUNT = 0
+
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='product_images', verbose_name='Фото')
+    sign = models.CharField(max_length=200, verbose_name='Подпись')
+    like_count = models.IntegerField(max_length=20, default=COUNT, verbose_name='Количество лайков')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='Автор',
+                             related_name='photo_author')
+
+    def __str__(self):
+        return self.sign
+
+
+# class Like(models.Model):
+#     user = models.ForeignKey('auth.User', related_name='likes', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=3000, verbose_name='Текст')
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, verbose_name='Фото', related_name='comment_photo')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='Автор',
+                               related_name='comment_author')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.text
+
+
+# class PhotoLike(models.Model):
+#     photo = models.ForeignKey(Photo, on_delete=models.CASCADE, verbose_name='Фото', related_name='photo_likes')
+#     like = models.ForeignKey(Like, on_delete=models.CASCADE, verbose_name='Лайк', related_name='photo_likes')
+#
+#     def __str__(self):
+#         return "{} - {}".format(self.photo, self.like)
+
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.shortcuts import render
+# from django.views.generic import ListView
+#
+# from webapp.models import Photo
+#
+#
+# class IndexView(ListView):
+#     model = Photo
+#     template_name = 'index.html'
+#
+#     def get_queryset(self):
+#         return Product.objects.filter(in_order=True)
+#
+#
+# class ProductView(StatsMixin, DetailView):
+#     model = Product
+#     template_name = 'product/detail.html'
+#
+#
+# class ProductCreateView(PermissionRequiredMixin, StatsMixin, CreateView):
+#     model = Product
+#     template_name = 'product/create.html'
+#     fields = ('name', 'category', 'price', 'photo', 'in_order')
+#     permission_required = 'webapp.add_product', 'webapp.can_have_piece_of_pizza'
+#     permission_denied_message = '403 Доступ запрещён!'
+#
+#     def get_success_url(self):
+#         return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+#
+#
+# class ProductUpdateView(LoginRequiredMixin, StatsMixin, UpdateView):
+#     model = Product
+#     template_name = 'product/update.html'
+#     fields = ('name', 'category', 'price', 'photo', 'in_order')
+#     context_object_name = 'product'
+#
+#     def get_success_url(self):
+#         return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+#
+#
+# class ProductDeleteView(LoginRequiredMixin, StatsMixin, DeleteView):
+#     model = Product
+#     template_name = 'product/delete.html'
+#     success_url = reverse_lazy('webapp:index')
+#     context_object_name = 'product'
+#
+#     def delete(self, request, *args, **kwargs):
+#         product = self.object = self.get_object()
+#         product.in_order = False
+#         product.save()
+#         return HttpResponseRedirect(self.get_success_url())
